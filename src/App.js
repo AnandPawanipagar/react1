1f50d2e5d6699bc51cd65e1c866b978abc311146
AnandPawanipagar <anandpawanipagar97@gmail.com>
2021-07-18 12:25:35 +0530

    index1 and index2 are added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-    return ( <
-        div >
-        <
-        div className = "myclass" > My name is Anand < /div> <
-        div className = "myclass1" > < /div> <
-        /div>
-    );
+  return (
+    <div>
+      <div className="myclass"> My name is Anand </div>
+      <div className="myclass1"> </div>{" "}
+    </div>
+  );
 }
 
 export default App;
